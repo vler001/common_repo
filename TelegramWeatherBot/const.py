b6d1e163f88ada00e7e3a5d7c0429c6fa3d5a300
vler001 <115975822+vler001@users.noreply.github.com>
2023-04-20 20:08:56 +0300

    Update file
    Update file

--- TelegramWeatherBot/const.py
+++ TelegramWeatherBot/const.py
@@ -1,13 +1,13 @@
 
 
-TOKEN = '5980276918:AAGFewzpI5pqg5DIf46-ncqsevybxgcWHyQ'
+TOKEN = 'uwyetwqteuiy2upioe'
 
 URL = 'https://api.telegram.org/bot{token}/{method}'
 
 UPDATE_METH = 'getUpdates'
 SEND_METH = 'sendMessage'
 
-MY_ID = 460886093
+MY_ID = 000000000
 
 
 UPDATE_ID_FILE_PATH = 'update_id'
@@ -19,7 +19,7 @@ with open(UPDATE_ID_FILE_PATH) as file:
     UPDATE_ID = data
 
 
-WEATHER_TOKEN = 'e05b1f1c06b5ad30ff8ba88079da1ca9'
+WEATHER_TOKEN = 'iwertqweryqwur[wupertiqpo[prwo'
 
 WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={token}'
 
